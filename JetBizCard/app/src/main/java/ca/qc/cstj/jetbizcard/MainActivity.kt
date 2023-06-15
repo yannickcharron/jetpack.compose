68558878bb3547822f17d63279c48bc2249fb5e8
Yannick Charron <yannick.charron@gmail.com>
2023-06-15 13:36:15 -0400

    Fin de l'application d'intro.

--- JetBizCard/app/src/main/java/ca/qc/cstj/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/ca/qc/cstj/jetbizcard/MainActivity.kt
@@ -1,21 +1,25 @@
 package ca.qc.cstj.jetbizcard
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,18 +32,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ca.qc.cstj.jetbizcard.ui.theme.JetBizCardTheme
 import ca.qc.cstj.jetbizcard.ui.theme.OffWhite
 
@@ -65,7 +70,9 @@ private fun CreateBizCard() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        Card(modifier = Modifier.fillMaxSize().padding(12.dp),
+        Card(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             colors = CardDefaults.cardColors(containerColor = OffWhite)
@@ -91,6 +98,9 @@ private fun CreateBizCard() {
 @Composable
 private fun CardInfoSection(modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val portfolioClickedState = remember {
+        mutableStateOf(false)
+    }
     Column(modifier =
         Modifier.padding(5.dp),
         horizontalAlignment = Alignment.CenterHorizontally) {
@@ -101,16 +111,19 @@ private fun CardInfoSection(modifier: Modifier = Modifier) {
             Text(text= "@yellowkiwi", modifier = Modifier.padding(3.dp), style = MaterialTheme.typography.labelSmall)
 
             Button(onClick = {
-                Toast.makeText(context, "Bonjour d'un bouton Compose", Toast.LENGTH_LONG).show()
+                //Toast.makeText(context, "Bonjour d'un bouton Compose", Toast.LENGTH_LONG).show()
+                portfolioClickedState.value = !portfolioClickedState.value
             }) {
                 Text(text = stringResource(R.string.portfolio), style = MaterialTheme.typography.labelMedium)
             }
+            if(portfolioClickedState.value) {
+                PorfoliosList()
+            } 
     }
 }
 
-@Preview
 @Composable
-private fun Content() {
+private fun PorfoliosList() {
     Box(modifier = Modifier
         .fillMaxSize()
         .padding(5.dp)) {
@@ -120,39 +133,63 @@ private fun Content() {
                 shape = RoundedCornerShape(corner = CornerSize(6.dp)),
                 border = BorderStroke(width = 2.dp, color = Color.LightGray)
             ) {
-                Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+                Portfolio(projects = listOf("Project 1", "Project 2", "Project 3", "Project 4", "Project 5"))
             }
     }
 }
 
 @Composable
-fun Portfolio(data: List<String>) {
-    Text(text= "Portfolio Here")
+fun Portfolio(projects: List<String>) {
+    LazyColumn {
+        items(projects) { project ->
+            PortfolioCardItem(project)
+        }
+    }
+}
+@Composable
+private fun PortfolioCardItem(projectTitle : String) {
+    Card(modifier = Modifier
+        .padding(8.dp),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(16.dp)) {
+            ProfilePicture(modifier = Modifier.size(100.dp))
+            Column(modifier = Modifier
+                .padding(8.dp)
+                .align(alignment = Alignment.CenterVertically),
+            ) {
+                Text(text =  projectTitle, fontWeight = FontWeight.Bold)
+                Text(text = "Description du projet", style = MaterialTheme.typography.labelSmall)
+            }
+        }
+    }
 }
 
 @Composable
 private fun ProfilePicture(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
         border = BorderStroke(0.5.dp, color = MaterialTheme.colorScheme.primary),
         shadowElevation = 4.dp, // Shadow vs Tonal to lookAt
         //color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
-
     ) {
         Image(
             painter = painterResource(id = R.drawable.kiwi),
             contentDescription = "",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
 }
 
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetBizCardTheme {
