2f25764db38b94cdc5feccfe77b241ebd00d185f
Yannick Charron <yannick.charron@gmail.com>
2023-10-31 16:07:44 -0400

    feature: Navigation

--- NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/NavigationApp.kt
+++ NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/NavigationApp.kt
@@ -1,12 +1,25 @@
 package ca.qc.cstj.navigationdemo
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import ca.qc.cstj.navigationdemo.ui.components.BottomNavItem
+import ca.qc.cstj.navigationdemo.ui.components.BottomNavigationBar
+import ca.qc.cstj.navigationdemo.ui.screens.analytics.AnalyticsScreen
 import ca.qc.cstj.navigationdemo.ui.screens.home.HomeScreen
+import ca.qc.cstj.navigationdemo.ui.screens.list.ListScreen
 import ca.qc.cstj.navigationdemo.ui.screens.login.LoginScreen
+import ca.qc.cstj.navigationdemo.ui.screens.profile.ProfileScreen
+
+//https://nameisjayant.medium.com/nested-navigation-in-jetpack-compose-597ecdc6eebb
 
 @Composable
 fun NavigationApp(
@@ -14,12 +27,41 @@ fun NavigationApp(
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Login.route,
-        builder = {
-            composable(Screen.Login.title, content =  { LoginScreen(navController) })
-            composable(Screen.Home.title, content =  { HomeScreen() })
+        startDestination = Screen.Login.route
+    ) {
+        composable(Screen.Login.route, content = { LoginScreen(navController) })
+        composable(Screen.Home.route) {
+            navigation(route = Screen.Home.title, startDestination = Screen.Profile.route) {
+                composable(Screen.Profile.route) {
+                    BottomNavScreen(navController) {
+                        ProfileScreen()
+                    }
+                }
+                composable(Screen.Analytics.route)  {
+                    BottomNavScreen(navController) {
+                        ProfileScreen()
+                    }
+                }
+                composable(Screen.List.route)  {
+                    BottomNavScreen(navController) {
+                        ProfileScreen()
+                    }
+                }
+            }
+        }
 
-    })
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomNavScreen(navController:NavHostController, content: @Composable () -> Unit) {
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+        content()
+    }
 }
 
 //sealed class Screens(@StringRes val title: Int) {
