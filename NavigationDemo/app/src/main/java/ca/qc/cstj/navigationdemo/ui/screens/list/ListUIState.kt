3853168df324363e2095df901c3a99a4db0c95bb
Yannick Charron <ycharron@cstj.qc.ca>
2023-12-19 09:40:27 -0500

    feature: LazyComponent

--- NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/ui/screens/list/ListUIState.kt
+++ NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/ui/screens/list/ListUIState.kt
@@ -1,3 +1,6 @@
 package ca.qc.cstj.navigationdemo.ui.screens.list
 
-data class ListUIState(val counter:Int = 0)
+data class ListUIState(
+    val counter:Int = 0,
+    var list:List<Int> = listOf()
+)
