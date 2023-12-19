c9fc5fca89fea1fb8334e237dc2f43220d05e365
Yannick Charron <ycharron@cstj.qc.ca>
2023-12-19 10:20:50 -0500

    feature: LazyComponent

--- NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/ui/screens/list/ListUIState.kt
+++ NavigationDemo/app/src/main/java/ca/qc/cstj/navigationdemo/ui/screens/list/ListUIState.kt
@@ -1,5 +1,8 @@
 package ca.qc.cstj.navigationdemo.ui.screens.list
 
+//https://cazimirroman.medium.com/sealed-classes-vs-data-classes-669446e8ed3b
+//https://medium.com/swlh/new-way-handle-state-event-in-android-with-sealed-classes-75ad63560574
+//https://betterprogramming.pub/how-to-use-kotlin-sealed-classes-for-state-management-c1cfb81abc6a
 data class ListUIState(
     val counter:Int = 0,
     var list:List<Int> = listOf()
